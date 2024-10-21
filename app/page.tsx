4941237357ea0a46abce84663d517897fc7f63e1
Yuminasann <ahmadmaulanan1952@gmail.com>
2024-10-21 10:55:07 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import FavColor from './components/favColor';
+import Hero from './components/hero';
+import RiwayatPendidikan from './components/riwayatPendidikan';
+
+export default function Gallery() {
+  return (
+    <section>
+      <Hero/>
+      <RiwayatPendidikan/>
+      <FavColor/>
+    </section>
+  );
+}
